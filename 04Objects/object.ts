1a8b9bb39ff834814e4e9742fe9eaa5deb0ba7a7
HarshJaiswal02 <harshjaiswal0204@gmail.com>
2024-03-02 00:28:15 +0530

    object extended

--- 04Objects/object.ts
+++ 04Objects/object.ts
@@ -42,7 +42,14 @@ function myFunction({ name, age }): { data: string; thing: number } {
   };
 }
 
-console.log(myFunction({ name: "Harsh", age: 21 }));
+// console.log(myFunction({ name: "Harsh", age: 21 }));
 
 // ! Odd behaviour?
+console.log(
+//? myFunction({ name: "Harsh", age: 21, email: "harshjais05@gmail.com" })
+); //throws error squiggly lines shows as no email field in parameter of function definition
+
+const usingRef = { name: "Harsh", age: 21, email: "harshjais05@gmail.com" };
+console.log(myFunction(usingRef)); 
+//no error even after passing extra information as email is not their but also we didn't destructure the email
 export {};
