a3c139b817e8eabc35bb13a1798695eeb732540e
HarshJaiswal02 <harshjaiswal0204@gmail.com>
2024-03-01 19:45:59 +0530

    Types : Primitive

--- 02Variables/variables.js
+++ 02Variables/variables.js
@@ -2,5 +2,25 @@
 // Syntax
 // let variable: type  =  value
 Object.defineProperty(exports, "__esModule", { value: true });
-var name = 6;
+var name = "Harsh ";
+// const age: String = 20  this line throw error if we compile {tsc variable.ts}
 console.log(name);
+// TypeScript Types
+// Number
+var price = 30;
+price.toFixed();
+// price = "" throws error
+//Boolean
+var isLogin = true;
+console.log(isLogin.valueOf());
+// Void null undefined
+var context = null;
+console.log(context);
+function sayMyName(myName) {
+    console.log("Hii ".concat(myName)); //function does not return any value then you can specify void as return type.
+}
+sayMyName("Harsh");
+var emptiness = sayMyName("Harsh");
+console.log(emptiness); //Output : undefined
+var unknown = undefined;
+console.log(unknown);
--- 02Variables/variables.ts
+++ 02Variables/variables.ts
@@ -1,10 +1,38 @@
 // Syntax
 // let variable: type  =  value
 
-let name: String = "Harsh ";
+let name: string = "Harsh ";
 
 // const age: String = 20  this line throw error if we compile {tsc variable.ts}
 
 console.log(name);
 
 export {};
+
+// TypeScript Types
+
+// Number
+
+let price: number = 30;
+price.toFixed();
+// price = "" throws error
+
+//Boolean
+let isLogin: boolean = true;
+
+console.log(isLogin.valueOf());
+
+// Void null undefined
+let context: null = null;
+console.log(context);
+
+function sayMyName(myName: string): void {
+  console.log(`Hii ${myName}`); //function does not return any value then you can specify void as return type.
+}
+let emptiness: void = sayMyName("Harsh");
+console.log(emptiness); //Output : undefined
+
+//https://www.tutorialsteacher.com/typescript/typescript-void
+
+let unknown: undefined = undefined;
+console.log(unknown);
