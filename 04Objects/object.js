a390932a5993678f0552d22936d6bc1ec92a8fa4
HarshJaiswal02 <harshjaiswal0204@gmail.com>
2024-03-02 01:03:51 +0530

    Topic Type alias keywords: readonly , optional , ampersand

--- 04Objects/object.js
+++ 04Objects/object.js
@@ -5,21 +5,35 @@
 // }
 Object.defineProperty(exports, "__esModule", { value: true });
 var User = {
-    name: "Harsh",
-    age: 21,
+  name: "Harsh",
+  age: 21,
 };
 console.log(User);
 var AnotherUser = {
-    name: "Another",
-    age: 11,
+  name: "Another",
+  age: 11,
 };
 console.log(AnotherUser);
-// ! Odd behaviour?
+// ? Function accepts an object as parameter[destructured] and returns an object with type safety
 function myFunction(_a) {
-    var name = _a.name, age = _a.age;
-    return {
-        data: name,
-        thing: age,
-    };
+  var name = _a.name,
+    age = _a.age;
+  return {
+    data: name,
+    thing: age,
+  };
 }
-console.log(myFunction({ name: "Harsh", age: 21 }));
+// console.log(myFunction({ name: "Harsh", age: 21 }));
+// ! Odd behaviour?
+console.log(); //throws error squiggly lines shows as no email field in parameter of function definition
+var usingRef = { name: "Harsh", age: 21, email: "harshjais05@gmail.com" };
+console.log(myFunction(usingRef));
+var user = {
+  _id: "13wsqd2d11s2qx12",
+  data: "MongodbData",
+  isValid: false,
+  salary: 50,
+};
+console.log(user);
+user.salary = 100;
+console.log(user);
--- 04Objects/object.ts
+++ 04Objects/object.ts
@@ -15,6 +15,7 @@
 //     age: 11,
 // }
 
+// Type alias  [alias means assumed ]
 type objecting = {
   name: string;
   age: number;
@@ -45,11 +46,50 @@ function myFunction({ name, age }): { data: string; thing: number } {
 // console.log(myFunction({ name: "Harsh", age: 21 }));
 
 // ! Odd behaviour?
-console.log(
-//? myFunction({ name: "Harsh", age: 21, email: "harshjais05@gmail.com" })
-); //throws error squiggly lines shows as no email field in parameter of function definition
+console
+  .log
+  //? myFunction({ name: "Harsh", age: 21, email: "harshjais05@gmail.com" })
+  (); //throws error squiggly lines shows as no email field in parameter of function definition
 
 const usingRef = { name: "Harsh", age: 21, email: "harshjais05@gmail.com" };
-console.log(myFunction(usingRef)); 
+console.log(myFunction(usingRef));
 //no error even after passing extra information as email is not their but also we didn't destructure the email
+
+// Readonly keyword
+// Optional ? keyword
+
+type User = {
+  readonly _id: string;
+  data: string;
+  isValid: boolean;
+  salary: number;
+  creditCard?: number;
+};
+
+let user: User = {
+  _id: "13wsqd2d11s2qx12",
+  data: "MongodbData",
+  isValid: false,
+  salary: 50,
+};
+
+console.log(user);
+// user._id = "ei90dscn2hhwe3nnn2w9" //cannot assigned readonly
+user.salary = 100;
+console.log(user);
+
+
+// Type alias mismatching or extension 
+
+type cardNumber = {
+    cardnumber:number;
+}
+type cardHolderName = {
+    holdername:string;
+}
+
+type cardDetail = cardHolderName & cardNumber & {
+    cvv:number
+}
+
 export {};
